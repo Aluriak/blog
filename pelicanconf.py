d05e43040682f6123a8b317af585978ec239038a
Aluriak <lucas.bourneuf@laposte.net>
2017-05-13 03:32:42 +0200

    conf: fix base url

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ sys.path.append('.')
 ################################################################################
 AUTHOR = 'lucas bourneuf'
 SITENAME = "lucas/blog"
-SERVERURL = 'http://lucas.bourneuf.net'
+SERVERURL = 'https://lucas.bourneuf.net'
 SITEURL = '/blog'
 
 PATH = 'content'
